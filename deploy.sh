3435c8a1886513081b66175f64b82488b33aae5c
Teodor B <teodor@bardici.ro>
2023-08-06 19:44:56 +0200

    bzip2 instead of bzip

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker save -o certman.tar certman | bzip | ssh heapzilla@192.168.2.77 docker load 
+docker save -o certman.tar certman | bzip2 | ssh heapzilla@192.168.2.77 docker load 
